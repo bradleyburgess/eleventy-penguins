76818e1bdcbf576aa22794380604f433e8ea47fd
Bradley Burgess <47909061+bradleyburgess@users.noreply.github.com>
2022-04-18 21:09:49 +0200

    update

--- 11ty/shortcodes/image.js
+++ 11ty/shortcodes/image.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes = "100vw") {
   let metadata = await Image(src, {
     widths: [600, 1200, 1800, 2400, 3000, null],
-    formats: ["webp", "jpeg"]
+    formats: ["webp", "jpeg"],
+    outputDir: path.join("_site", "img"),
+    urlPath: "/img/"
   });
 
   let imageAttributes = {
@@ -13,8 +16,9 @@ async function imageShortcode(src, alt, sizes = "100vw") {
     decoding: "async"
   };
 
+  console.log(metadata.webp[0]);
+
   const result = Image.generateHTML(metadata, imageAttributes);
-  console.log(metadata);
   return result;
 }
 
--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("_site"));
-app.listen(8080, () => {
-  console.log("Listening on 8080");
-});
